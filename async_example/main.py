089c7d869bee87848e63a9cd2e99121d8f0a8bdc
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-08-23 12:26:44 +0800

    Add sync and async endpoints

---
+++ async_example/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import time
+import asyncio
+
+app = FastAPI()
+
+@app.get("/sync")
+def read_sync():
+	time.sleep(2)
+	return {
+		"message": "Synchronus blocking endpoint"
+	}
+
+@app.get("/async")
+async def read_async():
+	await asyncio.sleep(2)
+	return {
+		"message":
+		"Asynchronus non-blocking endpoint"
+	}
