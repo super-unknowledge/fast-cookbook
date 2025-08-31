99e3da28ffd4aea4d6bf35479df782de7d1f7a38
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-08-31 12:03:49 +0800

    Add event class

--- ticketing_system/database.py
+++ ticketing_system/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import (
 	DeclarativeBase,
 	Mapped,
 	mapped_column,
+	relationship,
 )
 
 
@@ -17,3 +18,22 @@ class Ticket(Base):
 	price: Mapped[float] = mapped_column(nullable=True)
 	show: Mapped[str | None]
 	user: Mapped[str | None]
+# 	details: Mapped["TicketDetails"] = relationship(
+# 		back_populates="ticket"
+# 	)
+	event_id: Mapped[int | None] = mapped_column(
+		ForeignKey("events.id")
+	)
+	event: Mapped["Event | None"] = relationship(
+		back_populates="tickets"
+	)
+
+
+class Event(Base): 
+	__tablename__ = "events"
+	
+	id: Mapped[int] = mapped_column(primary_key=True)
+	name: Mapped[str]
+	tickets: Mapped[list["Ticket"]] = relationship(
+		back_populates="event"
+	)
